b28211db2ded99861e914beee8f094218545daad
Darya Shumikhina <shumikhina.darya@gmail.com>
2021-05-13 20:36:10 +0300

    first_change

--- main.py
+++ main.py
@@ -1,32 +1,22 @@
 from threading import Thread, Lock
-import typing
+
 
 a = 0
 
 
-def function(arg):
+def function(arg, lock):
     global a
     for _ in range(arg):
-        a += 1
+        with lock:
+            a += 1
     return a
-
-
-def function_lock(arg, lock: typing.TypeVar):
-    temp = 0
-
-    for _ in range(arg):
-        temp += 1
-
-    with lock:
-        global a
-        a += temp
 
 
 def main():
     threads = []
     lock = Lock()
     for i in range(5):
-        thread = Thread(target=function_lock, args=(1000000, lock))
+        thread = Thread(target=function, args=(1000000, lock))
         thread.start()
         threads.append(thread)
 
